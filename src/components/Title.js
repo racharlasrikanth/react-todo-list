5dcc3d8a6641545c52665a0d1e3873b57d7c9a2a
Srikanth <sri1@publicisgroupe.net>
2022-01-30 18:58:16 +0530

    title changed && few styles added

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = () => {
   return <div className="title">
-  <h2>todo container</h2>
+  <h2>todo list</h2>
   <div className="underline"></div>
 </div>;
 }
