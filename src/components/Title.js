ce07807a3b21c45c7a9ffdbea64fe0f8f030dcd6
Srikanth Racharla <racharlasrikanth3211@gmail.com>
2023-04-28 12:30:28 +0530

    some changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
-  return <div className="title">
-  <h2>todo list</h2>
-  <div className="underline"></div>
-</div>;
-}
+  return (
+    <div className="title">
+      <h2>todo list srikanth</h2>
+      <div className="underline"></div>
+    </div>
+  );
+};
 
 export default Title;
